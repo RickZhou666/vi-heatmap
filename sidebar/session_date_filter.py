e378d913e2e588dc881cbfcf8209fbecab2a4de7
RickZhou <runzhou666@gmail.com>
2025-07-27 20:51:50 -0700

    change default day to last 7 days

--- sidebar/session_date_filter.py
+++ sidebar/session_date_filter.py
@@ -1,10 +1,11 @@
 import streamlit as st
-from datetime import date
+from datetime import date, timedelta
 
 def display_sidebar_date_filters():
     current_date = date.today()
     start_date = current_date.replace(year=current_date.year - 1)
-    default_start = current_date.replace(day=1) if current_date.month == 1 else current_date.replace(month=current_date.month - 1)
+    # default_start = current_date.replace(day=1) if current_date.month == 1 else current_date.replace(month=current_date.month - 1)
+    default_start = current_date - timedelta(days=6)
     session_date_range = st.sidebar.slider(
         "Session Start Dt",
         value=(default_start, current_date),
